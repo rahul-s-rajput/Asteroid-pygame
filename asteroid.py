f142bbbeb706a7ffd03a049d50cdc6c976924091
rahul-s-rajput <151556413+rahul-s-rajput@users.noreply.github.com>
2025-02-28 17:59:38 -0800

    Adding asteroids and shooting mechanism

---
+++ asteroid.py
@@ -0,0 +1,26 @@
+from circleshape import CircleShape
+import pygame
+from constants import ASTEROID_MIN_RADIUS
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius > ASTEROID_MIN_RADIUS:
+            random_angle = random.uniform(20,50)
+            v1 = self.velocity.rotate(random_angle)
+            v2 = self.velocity.rotate(-random_angle)
+            a1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            a2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            a1.velocity = v1*1.2
+            a2.velocity = v2*1.2
+        
